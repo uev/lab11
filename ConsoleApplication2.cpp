1fd40bc7baf4fbc6b31309725a7e9922cea68fc8
unknown <uev5bsd@yahoo.com>
2013-01-06 11:34:43 +0400

    Stage 2

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -16,31 +16,38 @@
 #define SIZE 255
 
 void mysort(char **Array, int Size,int flag);
+void exchange(char ** SArray, int lseek,int rseek);
+
+void exchange(char ** SArray, int lseek,int rseek) { // обмен значений
+	char * tmp;
+	tmp = SArray[lseek];
+    SArray[lseek] = SArray[rseek];
+    SArray[rseek] = tmp;
+}
 
 void mysort(char **Array, int Size,int flag)
 {
 	/*
 	 * flag - exersize variable
 	 */
-        int i,j;
-        char *tmp;
-        for(i = 0; i<Size; i++){
+        int i=0,j,seek=0;
+		for(i = 0; i<Size; i++){
                 for(j = i+1; j<Size; j++){
                  //       printf("%s %s\n",Array[i],Array[j]);
-						if (flag == 0) {
-							if(strlen(Array[i])>strlen(Array[j])){
-								tmp = Array[i];
-                                Array[i] = Array[j];
-                                Array[j] = tmp;
+						while (Array[i][seek] == Array[j][seek]) {
+								seek+=1;
+							}
+						if (flag == 2) {
+							if(Array[i][seek]<Array[j][seek]){
+								exchange(Array,i,j);
 							 }
                         } else {
-                        	if(Array[i][0]> Array[j][0]){
-                        	     tmp = Array[i];
-                        	     Array[i] = Array[j];
-                        	     Array[j] = tmp;
+                        	if(Array[i][seek]> Array[j][seek]){
+								exchange(Array,i,j);
                         	}
                         }
-                }
+                seek=0;
+				}
         }
 }
 
@@ -69,12 +76,12 @@ int _tmain(int argc, _TCHAR* argv[])
 	}
     rewind(fp); // reset
     words = (char**)calloc(chk-1, sizeof(char*));
-    // 0 - сортировка по длине строк, 1 - сортировка по кодам символов
+    // 2 - сортировка по убыванию кодов символов, 1 - сортировка по возрастанию кодов символов
     fscanf(fp,"%s",g);
     if (g[0] == '1' && strlen(g) == 1){
 		flag+=1;
-    } else if (g[0] =='0' && strlen(g) == 1){
-		flag=0;
+    } else if (g[0] =='2' && strlen(g) == 1){
+		flag=2;
     } else{
 		puts("Error. Expected 0 or 1 on first line.");
         return -1;
